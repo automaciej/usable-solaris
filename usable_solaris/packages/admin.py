c6b9a20730fd0a341bd544d8a02d58cd0430051a
maciej.blizinski <maciej.blizinski@c418613c-f8e4-11dd-8259-6defae454439>
2009-02-24 08:45:01 +0000

    Added Patch model.

--- usable_solaris/packages/admin.py
+++ usable_solaris/packages/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from usable_solaris.packages.models import Package, PackageVersion
+from usable_solaris.packages.models import Package, PackageVersion, Patch
 from usable_solaris.packages.models import Machine, PackageInstallation
 
 class PackageAdmin(admin.ModelAdmin):
@@ -14,6 +14,10 @@ admin.site.register(PackageVersion, PackageVersionAdmin)
 class MachineAdmin(admin.ModelAdmin):
     list_display = ('fqdn', 'arch')
 admin.site.register(Machine, MachineAdmin)
+
+class PatchAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Patch, PatchAdmin)
 
 class PackageInstallationAdmin(admin.ModelAdmin):
     list_display = ('package_version', 'machine', 'inst_date', 'status', 'arch')
