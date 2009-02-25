2ec55225f6cb976b356671a5b8a1fb5897c9fc92
maciej.blizinski <maciej.blizinski@c418613c-f8e4-11dd-8259-6defae454439>
2009-02-25 10:09:41 +0000

    Added patch installations.

--- branches/package-enumeration/usable_solaris/solaris/packages.py
+++ branches/package-enumeration/usable_solaris/solaris/packages.py
@@ -1,15 +1,18 @@
-import subprocess
-import usable_solaris.packages.models as spm
 import antlr3
 import logging
 import operator
+import subprocess
+import usable_solaris.packages.models as spm
+
 logging.basicConfig(level=logging.DEBUG)
 
 try:
     from pkginfoParser import pkginfoParser
     from pkginfoLexer import pkginfoLexer
+    from showrevParser import showrevParser
+    from showrevLexer import showrevLexer
 except ImportError, e:
-    logging.fata("You need to generate pkginfoParser and pkginfoLexer "
+    logging.fata("You need to generate parsers and lexers "
                  "using ANTLRv3.")
     raise
 
@@ -32,6 +35,17 @@ class MachineEnumerator(object):
         return self.ast
 
     def Executify(self):
+        """Method name inspired by Stevey Yegge.
+
+        The Verb "execute", and its synonymous cousins "run", "start", "go",
+        "justDoIt", "makeItSo", and the like, can perform the work of any other
+        Verb by replacing it with an appropriate Executioner and a call to
+        execute(). Need to wait? Waiter.execute(). Brush your teeth?
+        ToothBrusher(myTeeth).go(). Take out the garbage?
+        TrashDisposalPlanExecutor.doIt().
+
+        http://tinyurl.com/kxubn
+        """
         ast = self.GetAst()
         self.PopulateDatabase(ast)
 
@@ -86,6 +100,51 @@ class MachineEnumerator(object):
                 pkg_inst.save()
 
 
+class PatchEnumerator(object):
+
+    def __init__(self, fqdn):
+        self.fqdn = fqdn
+        self.ast = None
+
+    def run(self):
+        ast = self.GetAst()
+
+    def GetAst(self):
+        if not self.ast:
+            filename = "%s.showrev" % self.fqdn
+            input_stream = open(filename, "r")
+            char_stream = antlr3.ANTLRInputStream(input_stream)
+            lexer = showrevLexer(char_stream)
+            tokens = antlr3.CommonTokenStream(lexer)
+            parser = showrevParser(tokens)
+            self.ast = parser.showrev()
+        return self.ast
+
+    def PopulateDatabase(self):
+        ast = self.GetAst()
+        tree = ast.tree
+        machine, machine_created = spm.Machine.objects.get_or_create(fqdn=self.fqdn)
+        for patch_ast in tree.children:
+            # [u'125096-15', u'Obsoletes', u'Requires', u'Incompatibles',
+            #         u'Packages']
+            patch_name = patch_ast.children[0].text
+            obsoletes_ast = patch_ast.children[1]
+            requires_ast = patch_ast.children[2]
+            incompatibles_ast = patch_ast.children[3]
+            packages_ast = patch_ast.children[4]
+            number_1, number_2 = [int(x) for x in patch_name.split("-")]
+            patch, patch_created = spm.Patch.objects.get_or_create(name=patch_name,
+                    defaults={
+                        'number_1': number_1,
+                        'number_2': number_2,
+                        })
+            try:
+                patch_installation = spm.PatchInstallation.objects.get(
+                        patch=patch, machine=machine)
+            except spm.PatchInstallation.DoesNotExist:
+                patch_installation = spm.PatchInstallation.objects.create(
+                        patch=patch, machine=machine)
+
 
 
 def main():
@@ -93,10 +152,11 @@ def main():
             'yaga.home.blizinski.pl',
             'vsol01.home.blizinski.pl',
             'lobelia.home.blizinski.pl',
-            'unavailable.home.blizinski.pl',
+            # 'unavailable.home.blizinski.pl',
     ]
-    pe = MachineEnumerator(machines[0])
-    pe.Executify()
+    for machine in machines:
+        me = MachineEnumerator(machines)
+        me.Executify()
 
 
 if __name__ == "__main__":
