10b0ce87822ce52657cdb577d860afeaad07be09
maciej.blizinski <maciej.blizinski@c418613c-f8e4-11dd-8259-6defae454439>
2009-03-28 17:07:04 +0000

    Added logging and easy parser execution.

--- branches/opencsw/packages/import_catalog.py
+++ branches/opencsw/packages/import_catalog.py
@@ -8,6 +8,8 @@ import antlr3
 import logging
 import urllib2
 
+logging.basicConfig(level=logging.DEBUG)
+
 try:
   from catalogParser import catalogParser
   from catalogLexer import catalogLexer
@@ -23,15 +25,20 @@ class CatalogImporter(object):
 
   def GetAst(self):
     if not self.ast:
+      logging.debug("Downloading the catalog from the web")
       catalog_url_d = urllib2.urlopen(
           "http://ftp.heanet.ie/pub/opencsw/current/i386/5.10/catalog")
+      logging.debug("Creating intermediate objects")
       char_stream = antlr3.ANTLRInputStream(catalog_url_d)
       lexer = catalogLexer(char_stream)
       tokens = antlr3.CommonTokenStream(lexer)
       parser = catalogParser(tokens)
+      logging.debug("Parsing...")
       self.ast = parser.catalog()
+      logging.debug("Parsing done.")
     return self.ast
 
 
 if __name__ == "__main__":
-    print "Please use classes defined in this package."
+  ci = CatalogImporter()
+  ast = ci.GetAst()
